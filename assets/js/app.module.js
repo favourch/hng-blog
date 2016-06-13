7f65eae1bf5a530de8aa138dbfb52dfdef5183b0
Neo Ighodaro <neo@creativitykills.co>
2016-06-13 18:34:17 +0100

    Added controller and modules.

--- assets/js/app.module.js
+++ assets/js/app.module.js
@@ -1,9 +1,25 @@
 (function () {
 	'use strict';
 
-	angular.module('HNGBlog', [
+	var app = angular.module('HNGBlog', [
+		'ui.router',
 		'HNGBlog.shared',
 		'HNGBlog.posts'
 	]);
+
+
+	app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+
+		// Fallback url...
+		$urlRouterProvider.otherwise("/");
+
+		// Routes...
+		$stateProvider
+			.state('home', {
+				url: "/",
+				controller: "PostsController",
+				templateUrl: "templates/posts.html"
+			});
+	}]);
 
 }());
